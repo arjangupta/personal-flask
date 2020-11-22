83f96cfe19b87f869aba6a761d540426a8d71fa2
Arjan Gupta <arjangupta95@gmail.com>
2020-11-22 17:15:51 -0600

    Update requirements for openssl

--- backend_api/api.py
+++ backend_api/api.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route('/home')
 def display_welcome_message():
     return "<p> Welcome to Arjan Gupta's personal website! </p> <p> This website is currently under construction. Please check back later. Thank you! <p>"
+
+if __name__ == "__main__":
+    app.run(ssl_context='adhoc')
