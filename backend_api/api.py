12ccb43208550b916da37feeae83690515424456
Arjan Gupta <arjangupta95@gmail.com>
2020-11-22 17:22:58 -0600

    Add 0.0.0.0 host to app.run

--- backend_api/api.py
+++ backend_api/api.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def display_welcome_message():
     return "<p> Welcome to Arjan Gupta's personal website! </p> <p> This website is currently under construction. Please check back later. Thank you! <p>"
 
 if __name__ == "__main__":
-    app.run(ssl_context='adhoc')
+    app.run(host='0.0.0.0')
